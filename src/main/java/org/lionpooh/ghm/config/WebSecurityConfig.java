bfc4bcb1b4fdf32d386c2f0ddc08efc73d252186
lionpooh <lionpooh37@gmail.com>
2020-02-25 12:22:24 +0900

    dev commit

--- src/main/java/org/lionpooh/ghm/config/WebSecurityConfig.java
+++ src/main/java/org/lionpooh/ghm/config/WebSecurityConfig.java
@@ -1,7 +1,10 @@
 package org.lionpooh.ghm.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,18 +14,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.
+
                 authorizeRequests(authorizeRequests -> authorizeRequests
                     .antMatchers("/images/**", "/js/**", "/css/**").permitAll()
                     .antMatchers("/join", "/verify").permitAll()
                     .anyRequest().authenticated()).
-                formLogin(formLogin -> formLogin
-                    .loginPage("/login")
-                    .permitAll());
+                formLogin(Customizer.withDefaults());
+//                formLogin(formLogin -> formLogin
+//                    .loginPage("/login")
+//                    .permitAll());
     }
 
-//    @Override
-//    public void configure(WebSecurity web) throws Exception {
-//        web.
-//                ignoring().antMatchers("/resource/static/**/*");
-//    }
+    @Override
+    public void configure(WebSecurity web)  {
+        // 해당 경로는 스프링 세큐리티 무시
+        web.
+                ignoring().antMatchers("/resources/static/**/*");
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider()
+//        auth.jdbcAuthentication().dataSource(null);
+//        super.configure(auth);
+    }
 }
--- src/main/java/org/lionpooh/ghm/controller/api/MainApiController.java
+++ src/main/java/org/lionpooh/ghm/controller/api/MainApiController.java
@@ -1,5 +1,6 @@
 package org.lionpooh.ghm.controller.api;
 
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
